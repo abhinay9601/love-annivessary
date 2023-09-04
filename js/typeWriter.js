b7844e1cf376f453a3b5e2c68afd58fbd7d7fed3
abhinay9601 <abhinaysingh372@gmail.com>
2023-09-04 11:17:59 +0530

    update

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,39 +1,42 @@
-
 let i = 0;
 let text1 = "Hey! Shital Rawat(Abhinay's Wife)";
-let text2 = " Remember I always love you❤️"
+let text2 = " Remember I always love you❤️ | I am always with you my love ❤️";
 let speed = 100;
-
-function typeWriter(text, para){
-	if(ok == 2){
-		clearInterval(typeInterval);
-	}
-	if(i < text.length){
-		document.getElementById(para).innerHTML += text.charAt(i);
-		i++;
-		speed = Math.random() * 50 + 100;
-	}
-	else{
-		if(ok == 0){
-			i = 0;
-		}
-		ok += 1;
-		
-	}
-	// document.getElementById("audio").play();
-
+let flag1 = true;
+function typeWriter(text, para) {
+  if (ok == 2) {
+    clearInterval(typeInterval);
+  }
+  if (i < text.length) {
+    if (text.charAt(i) == "|") {
+      flag1 = false;
+    } else {
+      if (flag1) {
+        document.getElementById(para).innerHTML += text.charAt(i);
+      } else {
+        document.getElementById("txt3").innerHTML += text.charAt(i);
+      }
+    }
+    i++;
+    speed = Math.random() * 50 + 100;
+  } else {
+    if (ok == 0) {
+      i = 0;
+    }
+    ok += 1;
+  }
+  // document.getElementById("audio").play();
 }
 
 var typeInterval;
 
 //window.onload = function() {
 //	window.onload = function(){};
-   	typeInterval = setInterval(function(){
-		if(ok == 0){
-			typeWriter(text1, "txt1");
-		}
-		else if(ok == 1){
-			typeWriter(text2, "txt2");
-		}
-	}, 100);
+typeInterval = setInterval(function () {
+  if (ok == 0) {
+    typeWriter(text1, "txt1");
+  } else if (ok == 1) {
+    typeWriter(text2, "txt2");
+  }
+}, 75);
 //};
